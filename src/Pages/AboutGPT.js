3f28ab8e5d75028a689d3256848e17657fc07f24
unknown <ajay.gaur@girlpowertalk.com>
2022-10-31 15:44:58 +0530

    changes

--- src/Pages/AboutGPT.js
+++ src/Pages/AboutGPT.js
@@ -2,6 +2,13 @@ import React from "react";
 import '../styles/aboutGPT.css'
 import gptIcon from '../images/gpt-icon.png'
 import aboutGptBanner from '../images/about-gpt-banner.png'
+import blog1 from "../images/press-blogs/blog1.png"
+import blog2 from "../images/press-blogs/blog2.png"
+import blog3 from "../images/press-blogs/blog3.png"
+import pic1 from '../images/about-gpt/pic1.png'
+import pic2 from '../images/about-gpt/pic2.png'
+import pic3 from '../images/about-gpt/pic3.png'
+import pic4 from '../images/about-gpt/pic4.png'
 
 const AboutGPT = () => {
   return (
@@ -20,38 +27,61 @@ const AboutGPT = () => {
       <div class="about-description about-gpt-description container">
         <div class="home-second-inner">
           <div class="row">
-            <div class="col-md-12 mt-md-0 mt-3 text-center">
+          <div class="col-md-3 mt-md-0 mt-3 text-start"> <img src={pic1}/> <img src={pic2}/></div>
+            <div class="col-md-6 mt-md-0 mt-3 text-center">
               <h2 className="about-gpt-heading"> About Girl Power Talk</h2>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae
-                ullamcorper consectetur ullamcorper porta aliquam hendrerit vel
-                netus in. Volutpat, vel fringilla pulvinar blandit turpis cum mi
-                imperdiet. Gravida sed euismod arcu faucibus tempus tempus.
-                Aliquet risus netus dolor, ac nulla. Est interdum integer purus
-                odio porttitor nullam. Maecenas ligula mauris diam aliquam dui,
-                morbi posuere augue. Ac nunc, egestas congue faucibus sit.
-                Malesuada egestas adipiscing ultricies id sit adipiscing egestas
-                iaculis duis. Aliquet ipsum, arcu blandit sapien elementum quis
-                sem. Vel odio elit nullam gravida. Tortor turpis sed lectus sit
-                donec aliquam ipsum ultricies. Maecenas consequat nec
-                scelerisque blandit cras mi ullamcorper lacus nibh. Elementum
-                pellentesque purus nibh feugiat non suspendisse lacus. Vivamus
-                nec orci in cras enim.
+              We believe that achieving true gender equality requires a radical shift in the way young women are mentored in the workplace. At Girl Power Talk, we are passionately dedicated to empowering women – as well as men and non-binary individuals – with opportunities to confidently learn, feel valued, and build a career full of purpose. Our talented team members gain a wealth of technical skills that supercharge their potential across both digital and traditional business landscapes. We are a meritocracy of hustlers that thrive without limits. Our nurturing culture balances critique with encouragement. Let’s unlock your potential. Welcome to Girl Power Talk.
               </p>
             </div>
-          </div>
-          <div class="row">
-            <div class="col-md-12 mt-md-0 mt-3 text-center">
-              <h2 className="awards-heading"> Awards</h2>
-              <p>
-                Rachita has been honored with several accolades in recognition
-                of her work to empower youth, provide opportunities to women,
-                and lead a revolutionaly enterprise.
-              </p>
-            </div>
+
+            <div class="col-md-3 mt-md-0 mt-3 text-end"> <img src={pic3}/> <img src={pic4}/></div>
           </div>
         </div>
       </div>
+
+      <section id="press-blog-section" >
+        <div className="container">
+          <h2>Press Blogs</h2>
+          <div className="row">
+            <div class="card col-md-4">
+              <div
+                className="card-img"
+                style={{ background: `url(${blog1})` }}
+              ></div>
+              <div class="card-body">
+                <p class="card-text blog-text">
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod”
+                </p>
+              </div>
+            </div>
+
+            <div class="card col-md-4">
+              <div
+                className="card-img"
+                style={{ background: `url(${blog2})` }}
+              ></div>
+              <div class="card-body">
+                <p class="card-text blog-text">
+                #WNCYA: The labels we fought to lose ft Rachita Sharma
+                </p>
+              </div>
+            </div>
+
+            <div class="card col-md-4">
+              <div
+                className="card-img"
+                style={{ background: `url(${blog3})` }}
+              ></div>
+              <div class="card-body">
+                <p class="card-text blog-text">
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod”
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
